a729e2aa21c6589438e39de7acbc71d3baec568f
youngrock <youngrock@mobilint.co>
2023-06-30 20:16:19 +0900

    Minor correct
    - Date, version

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ property["IP_Name_Header"]			= "Developers Guide"
 property["Ownership"]				= "Mobilint"
 property["Document_Name_Header"]	= "Developers Guide"
 property["Security_Level"]			= "Mobilint Confidential"
-property["Water_Mark"]				= "Provided to LAON ROAD, 29JUN23"
+property["Water_Mark"]				= "Provided to LAON ROAD, 30JUN23"
 
 AddRevision("0.2",	2021, 12,  01,	"Revised for v0.2")
 AddRevision("0.3",	2022, 02,  05,	"Revised for v0.3")
@@ -16,6 +16,6 @@ AddRevision("0.4",	2022, 02,  23,	"Revised for v0.4")
 AddRevision("0.5",	2022, 07,  01,	"Revised for v0.5")
 AddRevision("0.6",	2022, 08,  10,	"Revised for v0.6")
 AddRevision("0.7",  2023, 03,  23,  "Revised for v0.7")
-AddRevision("0.7.3",  2023, 06,  29,  "Revised for v0.7.7")
+AddRevision("0.7.7",  2023, 06,  30,  "Revised for v0.7.7")
 
 AddParagraph("[[src/body.md]]")
